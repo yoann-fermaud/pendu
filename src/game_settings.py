a1b5446ead9e1e19203b611663dda4bd7e674b23
yoann-fermaud <yoann.fermaud@laplateforme.io>
2023-02-06 16:17:23 +0100

    [ADD] common file

---
+++ src/game_settings.py
@@ -0,0 +1,17 @@
+import json
+import pygame
+
+pygame.init()
+
+pygame.display.set_caption("HANGMAN GAME")
+WIDTH, HEIGHT = 1280, 720
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+
+def get_font(size):
+    return pygame.font.Font("assets/font/font.ttf", size)
+
+
+def save_in_json(data):
+    with open("data_files/score.json", "w") as file:
+        json.dump(data, file, indent=4)
